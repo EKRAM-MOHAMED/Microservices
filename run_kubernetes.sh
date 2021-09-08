bf53bdf70cb18a494a2f3a0b0da62d0dc156b48d
EKRAM-MOHAMED <ekrammohamed2013@gmail.com>
2021-09-08 09:11:28 +0000

    k8s-out

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ekram321/microservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl create deployment udacity2 --image=$dockerpath --port=8000
 
+kubectl expose deployment udacity2 --type=NodePort
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+#kubectl expose deployment udacity2 --type=NodePort
+kubectl port-forward deployment/udacity2 8000:2000
